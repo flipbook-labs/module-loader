00f15e5fbf82feeebc286f3ed9681674e4764a18
David Minnerly <voxeldavid@gmail.com>
2022-01-03 11:29:28 -0800

    Add usage example for `bind`

--- src/bind.spec.lua
+++ src/bind.spec.lua
@@ -13,4 +13,24 @@ return function()
 
 		expect(callback()).to.equal("foo")
 	end)
+
+	it("should work for the usage example", function()
+		local Class = {}
+		Class.__index = Class
+
+		function Class.new()
+			local self = {}
+			self.value = "foo"
+			return setmetatable(self, Class)
+		end
+
+		function Class:getValue()
+			return self.value
+		end
+
+		local instance = Class.new()
+		local getValue = bind(instance, instance.getValue)
+
+		expect(getValue()).to.equal("foo")
+	end)
 end
