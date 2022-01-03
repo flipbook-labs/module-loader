96a729953b556238bbe034834c845e55bd3fd812
David Minnerly <voxeldavid@gmail.com>
2022-01-03 11:17:16 -0800

    Add documentation that breaks moonwave

--- src/bind.lua
+++ src/bind.lua
@@ -1,3 +1,30 @@
+--[=[
+	Binds an instance method so that it can be called like a function.
+
+	Usage:
+
+	```lua
+	local Class = {}
+	Class.__index = Class
+
+	function Class.new()
+		local self = {}
+		self.value = "foo"
+		return setmetatable(self, Class)
+	end
+
+	function Class:getValue()
+		return self.value
+	end
+
+	local instance = Class.new()
+	local getValue = bind(instance, instance.getValue)
+
+	print(getValue()) -- "foo"
+	```
+
+	@private
+]=]
 local function bind(self: table, callback)
 	return function(...)
 		return callback(self, ...)
