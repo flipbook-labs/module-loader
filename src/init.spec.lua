3d375af4f26c24885fd279d73599eb86452e3c05
David Minnerly <voxeldavid@gmail.com>
2022-01-03 10:45:24 -0800

    Add more tests

--- src/init.spec.lua
+++ src/init.spec.lua
@@ -2,9 +2,18 @@ return function()
 	local Mock = require(script.Parent.Parent.Mock)
 	local ModuleLoader = require(script.Parent)
 
-	local MOCK_MODULE = script.Parent.Mocks.module
+	local mockModuleInstance = script.Parent.Mocks.module
+	local mockModule = require(mockModuleInstance)
 
 	local loader: ModuleLoader.Class
+
+	local function countDict(dict: { [string]: any })
+		local count = 0
+		for _ in pairs(dict) do
+			count += 1
+		end
+		return count
+	end
 
 	beforeEach(function()
 		loader = ModuleLoader.new()
@@ -17,10 +26,10 @@ return function()
 		-- script context that an error will not be produced.
 		it("should return the Source property if it can be indexed", function()
 			local canIndex = pcall(function()
-				return MOCK_MODULE.Source
+				return mockModuleInstance.Source
 			end)
 
-			local source = loader:_getSource(MOCK_MODULE)
+			local source = loader:_getSource(mockModuleInstance)
 
 			if canIndex then
 				expect(source).to.be.ok()
@@ -30,9 +39,22 @@ return function()
 		end)
 	end)
 
-	describe("require", function()
-		FOCUS()
+	describe("cache", function()
+		beforeEach(function()
+			loader = ModuleLoader.new()
+		end)
 
+		it("should add a module and its source to the cache", function()
+			loader:cache(mockModuleInstance, mockModule)
+
+			local cachedModule = loader._cache[mockModuleInstance]
+
+			expect(cachedModule).to.be.ok()
+			expect(cachedModule[2]).to.equal(mockModule)
+		end)
+	end)
+
+	describe("require", function()
 		local mockLoadstring = Mock.new()
 
 		beforeEach(function()
@@ -51,13 +73,25 @@ return function()
 		end)
 
 		it("should use loadstring to load the module", function()
-			loader:require(MOCK_MODULE)
+			loader:require(mockModuleInstance)
 			expect(#mockLoadstring.mock.calls).to.equal(1)
 		end)
 
 		it("should add the module to the cache", function()
-			loader:require(MOCK_MODULE)
-			expect(loader._cache[MOCK_MODULE]).to.be.ok()
+			loader:require(mockModuleInstance)
+			expect(loader._cache[mockModuleInstance]).to.be.ok()
+		end)
+	end)
+
+	describe("clear", function()
+		it("should remove all modules from the cache", function()
+			loader:cache(mockModuleInstance, mockModule)
+
+			expect(countDict(loader._cache)).to.equal(1)
+
+			loader:clear()
+
+			expect(countDict(loader._cache)).to.equal(0)
 		end)
 	end)
 end
